6a900cff9f6b7009fb774b558c3630b1e5452de7
gamitej <gamitejpratapsingh998@gmail.com>
2024-10-15 12:02:37 +0530

    DONE : password checker TDD approach

--- src/app/pass_checker/PasswordChecker.ts
+++ src/app/pass_checker/PasswordChecker.ts
@@ -1,3 +1,31 @@
+export enum PasswordErrors {
+  SHORT = "Password must be at least",
+  NO_UPPER_CASE = "Upper case letters required",
+  NO_LOWER_CASE = "Lower case letters required",
+}
+
+export interface CheckResult {
+  valid: boolean;
+  reasons: PasswordErrors[];
+}
+
 export class PasswordChecker {
-  public checkPassword() {}
+  public checkPassword(password: string): CheckResult {
+    const reasons: PasswordErrors[] = [];
+
+    if (password.length < 8) reasons.push(PasswordErrors.SHORT);
+
+    // to check no uppercase
+    if (password === password.toLowerCase())
+      reasons.push(PasswordErrors.NO_UPPER_CASE);
+
+    // to check no lowercase
+    if (password === password.toUpperCase())
+      reasons.push(PasswordErrors.NO_LOWER_CASE);
+
+    return {
+      valid: reasons.length > 0 ? false : true,
+      reasons: reasons,
+    };
+  }
 }
