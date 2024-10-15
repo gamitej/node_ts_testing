9e76b6889f82e610c2e84cdd99b6b84da9ea72f7
gamitej <gamitejpratapsingh998@gmail.com>
2024-10-15 12:07:22 +0530

    REFACTOR : password checker

--- src/app/pass_checker/PasswordChecker.ts
+++ src/app/pass_checker/PasswordChecker.ts
@@ -13,19 +13,27 @@ export class PasswordChecker {
   public checkPassword(password: string): CheckResult {
     const reasons: PasswordErrors[] = [];
 
-    if (password.length < 8) reasons.push(PasswordErrors.SHORT);
-
-    // to check no uppercase
-    if (password === password.toLowerCase())
-      reasons.push(PasswordErrors.NO_UPPER_CASE);
-
-    // to check no lowercase
-    if (password === password.toUpperCase())
-      reasons.push(PasswordErrors.NO_LOWER_CASE);
+    this.checkForLength(password, reasons);
+    this.checkForUppercase(password, reasons);
+    this.checkForLowercase(password, reasons);
 
     return {
       valid: reasons.length > 0 ? false : true,
       reasons: reasons,
     };
+  }
+
+  private checkForLength(password: string, reasons: PasswordErrors[]) {
+    if (password.length < 8) reasons.push(PasswordErrors.SHORT);
+  }
+
+  private checkForUppercase(password: string, reasons: PasswordErrors[]) {
+    if (password === password.toLowerCase())
+      reasons.push(PasswordErrors.NO_UPPER_CASE);
+  }
+
+  private checkForLowercase(password: string, reasons: PasswordErrors[]) {
+    if (password === password.toUpperCase())
+      reasons.push(PasswordErrors.NO_LOWER_CASE);
   }
 }
